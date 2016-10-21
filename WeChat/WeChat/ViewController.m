2c8cd6842a574f60b897f6519280ad6585b665f4
卢江明 <550627296@qq.com>
2016-10-21 22:35:06 +0800

    开始写代码

--- WeChat/WeChat/ViewController.m
+++ WeChat/WeChat/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"开始写代码");
 }
 
 - (void)didReceiveMemoryWarning {
