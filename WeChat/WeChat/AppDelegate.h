d06ea3987a2f18940cbc63eab043ef70eba9069f
卢江明 <550627296@qq.com>
2016-10-21 22:27:07 +0800

    初始化项目

---
+++ WeChat/WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by admin on 16/10/21.
+//  Copyright © 2016年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
