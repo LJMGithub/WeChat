e88b95153089acb23056033b1440be975e0c73ab
卢江明 <550627296@qq.com>
2016-10-21 22:43:21 +0800

    添加了SecondVC类

---
+++ WeChat/WeChat/SecondVC.h
@@ -0,0 +1,13 @@
+//
+//  SecondVC.h
+//  WeChat
+//
+//  Created by admin on 16/10/21.
+//  Copyright © 2016年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondVC : UIViewController
+
+@end
